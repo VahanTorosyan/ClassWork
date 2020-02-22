e8bf1b6f3272d5f030c19b9c6397a11761a1eb7c
Vahan <vah.torosyan@gmail.com>
2020-02-22 17:53:17 +0400

    Initial Commit 22.02.2020

--- src/am/vtorosyan/Main.java
+++ src/am/vtorosyan/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int i = 0;
+        while (i < 5) {
+            System.out.println("A");
+            i = i + 1;
+        }
     }
 }
